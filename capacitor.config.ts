e0f25f10fdbc89126e3ae6a3a0c452fe686d9b52
Matin <mkomijani323@yahoo.com>
2025-08-04 09:55:30 +0330

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mizBunny',
+  webDir: 'dist'
+};
+
+export default config;
